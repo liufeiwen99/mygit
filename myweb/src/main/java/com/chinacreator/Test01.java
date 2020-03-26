57405e05b5a71275fed10a468fe7356b09813632
feiwen.liu <307698548@qq.com>
2020-03-26 19:12:27 +0800

    22222

--- myweb/src/main/java/com/chinacreator/Test01.java
+++ myweb/src/main/java/com/chinacreator/Test01.java
@@ -3,7 +3,7 @@ package com.chinacreator;
 public class Test01 {
 
 	public static void main(String[] args){
-		System.out.println("aaaaaaaaaaaaaaaaa");
+		System.out.println("aaaaaaaaaaaaaaaaa»·»·»ı»ı»Ù»Ù");
 	}
 	
 }

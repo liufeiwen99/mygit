2f8203467a424f6ef1a144b7f5cb0a13d96d5968
feiwen.liu <307698548@qq.com>
2020-03-26 19:04:55 +0800

    提交文件

--- myweb/src/main/java/com/chinacreator/Test01.java
+++ myweb/src/main/java/com/chinacreator/Test01.java
@@ -3,7 +3,7 @@ package com.chinacreator;
 public class Test01 {
 
 	public static void main(String[] args){
-		System.out.println("aaaaaaaaaaaaaaaaa\nbbbbbbbbbbbbbbbbbbb");
+		System.out.println("aaaaaaaaaaaaaaaaa");
 	}
 	
 }

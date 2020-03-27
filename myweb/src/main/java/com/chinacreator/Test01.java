08aa031541fc305f7c662414d8f42e8a7ed3d722
feiwen.liu <307698548@qq.com>
2020-03-27 14:42:53 +0800

    提交

--- myweb/src/main/java/com/chinacreator/Test01.java
+++ myweb/src/main/java/com/chinacreator/Test01.java
@@ -3,7 +3,7 @@ package com.chinacreator;
 public class Test01 {
 
 	public static void main(String[] args){
-		System.out.println("aaaaaaaaaaaaaaaaa»·»·»ı»ı»Ù»Ù");
+		System.out.println("aaaaaaaaaaaaaaaaa»·»·»ı»ı»Ù»Ù1111111");
 	}
 	
 }
